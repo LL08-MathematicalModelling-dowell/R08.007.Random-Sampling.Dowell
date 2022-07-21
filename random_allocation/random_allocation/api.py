4097df79c9dfc7357d9239c143a2411de44fe86f
Ritik Chahar <102214612+RitikChahar@users.noreply.github.com>
2022-07-21 19:05:21 +0530

    Update api.py

--- random_allocation/random_allocation/api.py
+++ random_allocation/random_allocation/api.py
@@ -9,7 +9,6 @@ def post_operation(request):
   import numpy as np
   import math
   from math import sqrt, sin, cos, pi, floor, ceil
-  import pandas as pd
   import matplotlib.pyplot as plt
   import random
   import base64
