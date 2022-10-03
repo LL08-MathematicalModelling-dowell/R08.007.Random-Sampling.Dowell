d0cb0bd87b3303d2bdd8deb49e0b824c7133db68
Ritik Chahar <102214612+RitikChahar@users.noreply.github.com>
2022-10-03 12:28:47 +0530

    Added an option to enter either Radius or N

--- FieldRP.cpp
+++ FieldRP.cpp
@@ -6,13 +6,27 @@ using namespace std;
 #include <time.h>
 float PI = 3.14;
 
-auto FieldRP(int columns, int rows, int selection, int N)
+auto FieldRP(int side, int selection, int choice, float value)
 {
-    int area = columns * rows;
-    int area_of_circle = area / N;
-    float radius = sqrt(area_of_circle / PI);
+    int area = side * side;
+    float radius;
+    int N;
+    if (choice == 0)
+    {
+        N = static_cast<int>(value);
+        int area_of_circle = area / N;
+        radius = sqrt(area_of_circle / PI);
+    }
+    else if (choice == 1)
+    {
+        radius = value;
+        int area_of_circle = PI * radius * radius;
+        N = round(area / area_of_circle);
+    }
+    else
+    {
+    }
     float diameter = 2 * radius;
-
     std::vector<std::pair<int, int>> coordinates;
 
     if (selection == 1)
@@ -21,35 +35,35 @@ auto FieldRP(int columns, int rows, int selection, int N)
     }
     if (selection == 2)
     {
-        coordinates.push_back({-columns / 2, rows / 2});
+        coordinates.push_back({-side / 2, side / 2});
     }
     if (selection == 3)
     {
-        coordinates.push_back({-columns / 2, 0});
+        coordinates.push_back({-side / 2, 0});
     }
     if (selection == 4)
     {
-        coordinates.push_back({-columns / 2, -rows / 2});
+        coordinates.push_back({-side / 2, -side / 2});
     }
     if (selection == 5)
     {
-        coordinates.push_back({0, -rows / 2});
+        coordinates.push_back({0, -side / 2});
     }
     if (selection == 6)
     {
-        coordinates.push_back({columns / 2, -rows / 2});
+        coordinates.push_back({side / 2, -side / 2});
     }
     if (selection == 7)
     {
-        coordinates.push_back({columns / 2, 0});
+        coordinates.push_back({side / 2, 0});
     }
     if (selection == 8)
     {
-        coordinates.push_back({columns / 2, rows / 2});
+        coordinates.push_back({side / 2, side / 2});
     }
     if (selection == 9)
     {
-        coordinates.push_back({0, rows / 2});
+        coordinates.push_back({0, side / 2});
     }
 
     std::vector<std::pair<int, int>> L;
@@ -73,7 +87,7 @@ auto FieldRP(int columns, int rows, int selection, int N)
         {
             int x = L[k].first + selectA;
             int y = L[k].second + selectB;
-            if (x >= -columns / 2 && y >= -rows / 2 && x <= columns / 2 && y <= rows / 2)
+            if (x >= -side / 2 && y >= -side / 2 && x <= side / 2 && y <= side / 2)
             {
                 X1.push_back({x, y});
             }
@@ -88,11 +102,14 @@ auto FieldRP(int columns, int rows, int selection, int N)
     return coordinates;
 }
 
-int main()
-{
-    auto coordinates = FieldRP(10000000, 10000000, 2, 20);
-    for (int k = 0; k < coordinates.size(); k++){
-        cout << "(" << coordinates[k].first << "," << coordinates[k].second << ")\n";
-    }
-    return 0;
-}
+// int main()
+// {
+//     int a, b, c, d;
+//     cin >> a >> b >> c >> d;
+//     auto coordinates = FieldRP(a, b, c, d);
+//     for (int k = 0; k < coordinates.size(); k++)
+//     {
+//         cout << "(" << coordinates[k].first << "," << coordinates[k].second << ")\n";
+//     }
+//     return 0;
+// }
