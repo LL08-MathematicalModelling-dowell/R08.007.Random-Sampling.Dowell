a6d2478bfb7e102a96ec35f5b34072488566822f
RitikChahar <ritickchahar@gmail.com>
2023-03-28 02:05:47 +0530

    Fixed logic for Field Random Point Function

--- FieldRP.cpp
+++ FieldRP.cpp
@@ -1,13 +1,53 @@
-#include <iostream>
-using namespace std;
-#include <cmath>
+// #include <pybind11/pybind11.h>
+// #include <pybind11/stl.h>
 #include <vector>
+#include <cmath>
 #include <cstdlib>
 #include <time.h>
+#include <algorithm>
+#include <iostream>
+#include <random>
+using namespace std;
+
 float PI = 3.14;
 
-auto FieldRP(int side, int selection, int choice, float value)
+// namespace py = pybind11;
+
+// Function to check if a point falls inside or on the circumference of a circle
+bool is_point_inside_circle(double px, double py, double cx, double cy, double radius)
 {
+    double distance_squared = (px - cx) * (px - cx) + (py - cy) * (py - cy); // calculate the distance between the point and the center of the circle
+    double radius_squared = radius * radius;                                 // calculate the square of the radius of the circle
+    if (distance_squared <= radius_squared)
+    { // if the distance is less than or equal to the square of the radius, the point is inside or on the circumference of the circle
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool is_point_inside_canvas(double px, double py, int side)
+{
+    if (px >= -side / 2 && py >= -side / 2 && px <= side / 2 && py <= side / 2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+struct PointVector
+{
+    int x, y;
+};
+
+std::vector<PointVector> FieldRP(int side, int selection, int choice, float value)
+{
+
     int area = side * side;
     float radius;
     int N;
@@ -27,7 +67,8 @@ auto FieldRP(int side, int selection, int choice, float value)
     {
     }
     float diameter = 2 * radius;
-    std::vector<std::pair<int, int>> coordinates;
+
+    std::vector<PointVector> coordinates;
 
     if (selection == 1)
     {
@@ -66,37 +107,33 @@ auto FieldRP(int side, int selection, int choice, float value)
         coordinates.push_back({0, side / 2});
     }
 
-    std::vector<std::pair<int, int>> L;
-    float i = 0.0;
-    while (i <= 2 * PI)
-    {
-        int a = round(diameter * cos(i));
-        int b = round(diameter * sin(i));
-        L.push_back({a, b});
-        i += PI / 6;
-    }
-
-    int selectA = coordinates[0].first;
-    int selectB = coordinates[0].second;
+    int selectA = coordinates[0].x;
+    int selectB = coordinates[0].y;
 
     srand(time(0));
     while (coordinates.size() < N)
     {
-        std::vector<std::pair<int, int>> X1;
-        for (int k = 0; k < L.size(); k++)
-        {
-            int x = L[k].first + selectA;
-            int y = L[k].second + selectB;
-            if (x >= -side / 2 && y >= -side / 2 && x <= side / 2 && y <= side / 2)
-            {
-                X1.push_back({x, y});
-            }
-        }
+        // Use a random device to seed the random number generator
+        std::random_device rd;
+        std::mt19937 gen(rd());
 
-        int random = (rand() % X1.size());
-        selectA = X1[random].first;
-        selectB = X1[random].second;
-        coordinates.push_back({selectA, selectB});
+        // Use a uniform distribution to select a random angle
+        std::uniform_real_distribution<double> dis(0.0, 2.0 * M_PI);
+        double theta = dis(gen);
+
+        // Calculate the corresponding point on the circumference of the circle
+        int x = selectA + diameter * cos(theta);
+        int y = selectB + diameter * sin(theta);
+        if ((is_point_inside_circle(x, y, selectA, selectB, radius) == false) && is_point_inside_canvas(x, y, side) == true)
+        {
+
+            coordinates.push_back({x, y});
+            selectA = x;
+            selectB = y;
+        }
+        else
+        {
+        }
     }
 
     return coordinates;
@@ -106,10 +143,10 @@ auto FieldRP(int side, int selection, int choice, float value)
 // {
 //     int a, b, c, d;
 //     cin >> a >> b >> c >> d;
-//     auto coordinates = FieldRP(a, b, c, d);
+//     std:vector<PointVector> coordinates = FieldRP(a, b, c, d);
 //     for (int k = 0; k < coordinates.size(); k++)
 //     {
-//         cout << "(" << coordinates[k].first << "," << coordinates[k].second << ")\n";
+//         cout << "(" << coordinates[k].x << "," << coordinates[k].y << ")\n";
 //     }
 //     return 0;
 // }
