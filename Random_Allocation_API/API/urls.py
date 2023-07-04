003fffdcf474c88ad10d5a53e47a08593d05f667
RitikChahar <ritickchahar@gmail.com>
2023-07-04 23:08:17 +0530

    Added django cors-headers

--- Random_Allocation_API/API/urls.py
+++ Random_Allocation_API/API/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('function_type/', views.function_type),
-    path('random_point_allocation_input/', views.random_point_allocation_input),
-    path('api/', views.random_allocation),
-
+    path('fieldrp/', views.FieldRP),
+    path('excelrp/', views.ExcelRP),
 ]
