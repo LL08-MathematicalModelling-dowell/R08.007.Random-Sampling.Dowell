fc51354fa94b8f59665dfc7b64fbe793bc0cc2b9
Githaiga <pythongeeko@gmail.com>
2023-10-31 17:27:15 +0300

    update dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/100107-DowellEmailExtractor/",
   plugins: [react()],
 })
