09820c160cd90dfdafa100009a9a67740c86a070
kidus <kidusabebe101@gmail.com>
2023-10-31 06:46:24 +0300

    Finished UI Design For all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ContactUs from "./components/ContactUs";
+import DynamicForm from "./components/DynamicForm";
 
 const App = () => {
   return (
+    <>
     <ContactUs/>
+    <DynamicForm/>
+    </>
   );
 };
 
--- src/components/DynamicForm.jsx
+++ src/components/DynamicForm.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 const DynamicForm = () => {
   return (
-    <section class="w-full h-full bg-slate-200">
-      <div class="py-8 lg:py-16  mx-auto ove bg-white max-w-screen-md">
+    <section class="w-full h-full bg-slate-100">
+      <div class=" mx-auto  bg-white max-w-[800px]">
         <form action="#" class="space-y-8">
+
           <div>
             <label
-              for="email"
-              class="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300"
+              for="text"
+              class="block ml-3 mb-3 text-lg font-medium text-gray-900 dark:text-gray-300"
             >
               Title
             </label>
+
             <input
               type="text"
               id="text"
@@ -19,27 +21,28 @@ const DynamicForm = () => {
               placeholder="Enter Title Here "
               required
             />
+
           </div>
 
           <div class="sm:col-span-2">
             <label
               for="message"
-              class="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400"
+              class="block ml-3 mb-3 text-lg font-medium text-gray-900 dark:text-gray-400 "
             >
               Enter Your message Here
             </label>
             <textarea
               id="message"
-              rows="6"
-              class="block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Leave a comment..."
+              rows="3"
+              class="block p-3   w-full text-lg text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
+              placeholder="Leave Your Message Here."
             ></textarea>
           </div>
 
           <div>
             <label
               for="email"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              class="block ml-3 mb-3 text-lg font-medium text-gray-900 dark:text-gray-300"
             >
               Your email
             </label>
@@ -47,7 +50,7 @@ const DynamicForm = () => {
               type="email"
               id="email"
               class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-              placeholder="name@flowbite.com"
+              placeholder="Enter Your Email Here"
               required
             />
           </div>
