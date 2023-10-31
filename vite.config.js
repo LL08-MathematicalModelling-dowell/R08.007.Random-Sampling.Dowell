b63718e3d05cb618a6878b42d46385cda7331d8c
Githaiga <pythongeeko@gmail.com>
2023-10-31 18:05:49 +0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: "/",
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/100107-DowellEmailExtractor/'
-  }
-
-  return config
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
 })
