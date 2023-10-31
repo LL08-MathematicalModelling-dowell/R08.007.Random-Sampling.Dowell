51a5698f092659899e50935d7734b9894a568c53
Githaiga <pythongeeko@gmail.com>
2023-10-31 17:45:13 +0300

    add browser router

--- src/PageRouter.jsx
+++ src/PageRouter.jsx
@@ -1,11 +1,15 @@
-import { Route, Routes as Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
 import Home from "./components/Home";
 
 const PageRouter = () => {
   return (
+    <BrowserRouter
+      basename={import.meta.env.DEV ? '/' : '/100107-DowellEmailExtractor/'}
+    >
     <Switch>
       <Route path="/" element={<Home />} />
     </Switch>
+    </BrowserRouter>
   );
 };
 
