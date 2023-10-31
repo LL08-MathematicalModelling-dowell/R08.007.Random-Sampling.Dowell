dea58c4e450e37b4eb1f6384449f304ea539b8c3
Githaiga <pythongeeko@gmail.com>
2023-10-31 18:02:00 +0300

    update vite config

--- src/PageRouter.jsx
+++ src/PageRouter.jsx
@@ -1,15 +1,12 @@
-import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
+import { Route, Routes as Switch } from "react-router-dom";
 import Home from "./components/Home";
 
 const PageRouter = () => {
   return (
-    <BrowserRouter
-      basename={import.meta.env.DEV ? '/' : '/100107-DowellEmailExtractor/'}
-    >
+    
     <Switch>
       <Route path="/" element={<Home />} />
     </Switch>
-    </BrowserRouter>
   );
 };
 
