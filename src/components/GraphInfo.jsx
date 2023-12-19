410c3e1ec078bb5986e12c38cdb4b86f63100c49
abatetsegaye1 <abatetsegaye12@gmail.com>
2023-12-20 00:46:53 +0300

    first message

--- src/components/GraphInfo.jsx
+++ src/components/GraphInfo.jsx
@@ -12,17 +12,32 @@ const GraphInfo = () => {
   const [generating,setGenerating]=useState(false);
   const [data, setData] = useState(null);
   const [error,setError]=useState(false);
+  const [selectedOption, setSelectedOption] = useState('fieldrp');
+  console.log("option",selectedOption)
+  const handleRadioChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
   const handleButtonClick =  async () => {
     setGenerating(true);
    
     try {
-      const response = await axios.post('http://100022.pythonanywhere.com/fieldrp', 
+      let response=null;
+      if(selectedOption==='fieldrp'){
+      response = await axios.post('http://100022.pythonanywhere.com/fieldrp/', 
         {
           side: side,
           selection: selection,
           choice:choice,
           value: value
         });
+      } else{
+        response = await axios.post('http://100022.pythonanywhere.com/excelrp/', 
+        {
+          side: side,
+          selection: selection,
+          
+        });
+      }
         console.log("data",response.data);
         console.log("data points",response.data.listOfPoints);
       setGenerating(false);
@@ -42,13 +57,28 @@ const GraphInfo = () => {
   return (
     <div className='flex justify-evenly'>
     <div className="max-w-sm mx-left mt-8 p-6 bg-white rounded-md shadow-md">
-      <h2 className="text-2xl font-semibold mb-4">Graph Information</h2>
+      <h2 className="text-2xl text-[#005734] font-semibold mb-4">Graph Information</h2>
+      <div className='flex justify-between w-full mb-2 mt-1'>
+      <label>
+        <input type="radio" className={`border-none checked:bg-green-500 text-green-300`}
+         checked={selectedOption === 'fieldrp'}
+        name="fileType" value="fieldrp" onChange={handleRadioChange}
+/> Field RP
+      </label>
+      <label>
+        <input type="radio" className={`border-none checked:bg-green-500 text-green-300`} name="fileType" value="exelrp" 
+        checked={selectedOption === 'exelrp'}
+        onChange={handleRadioChange}
+/> Excel RP
+      </label>
+    </div>
+
 
       <div className="mb-4">
         <label className="block text-sm font-medium text-gray-600">Side:</label>
         <input
           type="number"
-          className="mt-1 p-2 border rounded-md w-full"
+          className="mt-1 p-1 border rounded-md w-full  border-gray-300 flex mb-3 bg-gray-50 focus:outline-none text-gray-900 focus:border-[#005734]"
           value={side}
           onChange={(e) => setSide(Number(e.target.value))}
         />
@@ -58,34 +88,34 @@ const GraphInfo = () => {
         <label className="block text-sm font-medium text-gray-600">Selection:</label>
         <input
           type="number"
-          className="mt-1 p-2 border rounded-md w-full"
+          className="mt-1 p-1 border rounded-md w-full  border-gray-300 flex mb-3 bg-gray-50 focus:outline-none text-gray-900 focus:border-[#005734]"
           value={selection}
           onChange={(e) => setSelection(Number(e.target.value))}
         />
       </div>
 
-      <div className="mb-4">
+      {selectedOption !== 'exelrp' && (<div className="mb-4">
         <label className="block text-sm font-medium text-gray-600">Choice:</label>
         <input
           type="number"
-          className="mt-1 p-2 border rounded-md w-full"
+          className="mt-1 p-1 border rounded-md w-full  border-gray-300 flex mb-3 bg-gray-50 focus:outline-none text-gray-900 focus:border-[#005734]"
           value={choice}
           onChange={(e) => setChoice(Number(e.target.value))}
         />
-      </div>
+      </div>)}
 
-      <div className="mb-4">
+      {selectedOption !== 'exelrp' &&(<div className="mb-4">
         <label className="block text-sm font-medium text-gray-600">Value:</label>
         <input
           type="number"
-          className="mt-1 p-2 border rounded-md w-full"
+          className="mt-1 p-1 border rounded-md w-full  border-gray-300 flex mb-3 bg-gray-50 focus:outline-none text-gray-900 focus:border-[#005734]"
           value={value}
           onChange={(e) => setValue(Number(e.target.value))}
         />
-      </div>
+      </div>)}
 
       <button
-        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 w-full"
+        className="bg-green-700 hover:bg-green-600 text-white px-4 py-2 rounded-md mt-2 w-full"
         onClick={handleButtonClick}
       >
         Generate Random Graph
@@ -93,12 +123,12 @@ const GraphInfo = () => {
       <p>{generating ? 
                 <><Spinner/> <span className='text-green-400'>generating...</span></>  : ""}</p>
     </div>
-    <div className='max-w-sm mx-right mt-8 flex-1 p-6 bg-white rounded-md'>
-     {!show && <RandomGraph data={data}/>}
+    <div className='max-w-sm mx-right mt-8 flex-1 p-3 bg-white rounded-md'>
+     {show && <RandomGraph data={data}/>}
      {error && <p className='text-green-300'>Couldn &apos;t generate the graph </p>}
     </div>
     </div>
-    
+
   );
       }
 
--- src/components/RandomGraph.jsx
+++ src/components/RandomGraph.jsx
@@ -1,78 +1,83 @@
 
 import { Chart} from 'react-google-charts';
 
-/*
-## tested data for the graph##
-*/
-const data= [
-    [
-      0,
-      -50
-    ],
-    [
-      -35.673002392284054,
-      -49.17753720129195
-    ],
-    [
-      15.786318905249376,
-      -17.999505001376477
-    ],
-    [
-      32.90476906232237,
-      -36.19725687805518
-    ],
-    [
-      -30.40997593018656,
-      -31.332570914541105
-    ],
-    [
-      5.974991327546126,
-      -14.821327151654636
-    ],
-    [
-      29.99040382499477,
-      -30.665698276149154
-    ],
-    [
-      13.43904552010463,
-      -16.945021551304585
-    ],
-    [
-      25.96788429923947,
-      -25.527392256719452
-    ],
-    [
-      21.876765721316072,
-      -21.81052206390099
-    ]
-  ]
 
+function findMinMax(dataArray,axis) {
+  if (!dataArray || dataArray.length === 0) {
+    return null;
+  }
 
+  let min = Number.MAX_VALUE;
+  let max = Number.MIN_VALUE;
+
+  // Iterate through the outer array
+  for (const innerArray of dataArray) {
+    // Check if the inner array is present and not empty
+    if (innerArray && innerArray.length > 0) {
+      // Iterate through the inner array using destructuring
+      if(axis==='x') {
+        // Update min and max values
+        if (innerArray[0] < min) {
+          min = innerArray[0] ;
+        }
+
+        if (innerArray[0] > max) {
+          max = innerArray[0];
+        }
+      }
+      if(axis==='y') {
+        // Update min and max values
+        if (innerArray[1] < min) {
+          min = innerArray[1] ;
+        }
+
+        if (innerArray[1] > max) {
+          max = innerArray[1];
+        }
+      } 
+    }
+  }
+
+  return [min, max];
+}
 
 // eslint-disable-next-line react/prop-types
-const RandomGraph=()=> {
+const RandomGraph=({data})=> {
+ const [minX,maxX]=findMinMax(data,'x');
+ const [minY,maxY]=findMinMax(data,'y');
+ // eslint-disable-next-line react/prop-types
+ const xAxisValues=data.map((datas)=>parseInt(datas[0]));
+ // Find two lowest and two highest values
+ const sortedXValues = xAxisValues.slice().sort((a, b) => a - b);
+ const oneLowest = sortedXValues.slice(0, 1);
+ const oneHighest = sortedXValues.slice(-1);
+const tickedvalue=[...oneLowest,...oneHighest];
+console.log("ticked",tickedvalue);
   return (
+    <>
+    <h3 className='text-1xl text-[#005734] font-semibold text-center mb-1'>Random Chart</h3>
     <Chart
       chartType="LineChart"
       data={[["X","Y"],...data]}
       width="100%"
       height="400px"
       options={{
-        title: 'Random Chart',
+       
         curveType: 'function',
         
         hAxis: {
           title: 'X-Axis',
-          color:'transparent',
+        
           baseline: 0,
           gridlines: {
            // color:'transparent',
             count: -1, // -1 to display default number of gridlines
           },
           viewWindow: {
-            min: -65, // Adjust the min value based on your data
-            max: 65,  // Adjust the max value based on your data
+            min: minX>0?-50:minX-50, // Adjust the min value based on your data
+            max: maxX < 0 ?50:maxX+50,  // Adjust the max value based on your data
           },
+          ticks: tickedvalue,
         },
         vAxis: {
           baseline: 0,
@@ -83,14 +88,17 @@ const RandomGraph=()=> {
             count: -1, // -1 to display default number of gridlines
           },
           viewWindow: {
-            min: -65, // Adjust the min value based on your data
-            max: 65,  // Adjust the max value based on your data
+            min: minY>0?-40:minY-40, // Adjust the min value based on your data
+            max: maxY < 0 ?40:maxY+40,  // Adjust the max value based on your data
           },
         },
         legend: 'none',
+        colors: ['green'],
+        backgroundColor:'rgb(229 231 235)'
       }}    
 
     />
+    </>
   );
 };
 
